cb7a68b4ba493601da34257ce493015ee75388a9
Saurabh Mishra <Saurabh_mishra@epam.com>
2020-04-19 18:54:12 +0530

    updates

---
+++ src/main/java/com/epam/wfhmanager/wfhmanager/model/UserInfo.java
@@ -0,0 +1,25 @@
+package com.epam.wfhmanager.wfhmanager.model;
+
+public class UserInfo {
+
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    private String emailId;
+
+}
